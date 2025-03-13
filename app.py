44bc459d97decb3aab0f907fa7e699ba2a16c5e8
guy <guymeltzer28@gmail.com>
2025-03-13 13:19:58 +0200

    yolo5 app in k8s

--- app.py
+++ app.py
@@ -68,7 +68,7 @@ s3_client = boto3.client(
 # --- MongoDB Connection with Retry ---
 def connect_to_mongo():
     """Connect to MongoDB using URI from AWS Secrets Manager."""
-    mongo_uri = "mongodb://mongodb-0.mongodb:27017,mongodb-1.mongodb:27017,mongodb-2.mongodb:27017/default_db?replicaSet=rs0"
+    mongo_uri = "mongodb://mongodb.mongodb.svc.cluster.local:27017/?replicaset=rs0"
     # Fetch directly from loaded secrets
 
     if not mongo_uri:
